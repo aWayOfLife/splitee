9fcb602ccd196ca9a4946ad956c59d0cbcc73f8c
Kingshuk <saha.kingshuk@yahoo.com>
2021-07-31 16:15:01 +0530

    Setup project structure with scss

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/_global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
