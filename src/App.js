25c7675ce73110c6314308b533f18e862ad43db4
Kingshuk <saha.kingshuk@yahoo.com>
2021-08-01 03:18:10 +0530

    Poc complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { Geolocation } from '@capacitor/geolocation';
+import React, {useState, useCallback} from 'react';
+
+
 function App() {
+  const [loc, setLoc] = useState(null);
+  let coordinates = null;
+  const getCurrentPosition = useCallback(async () => {
+    coordinates = await Geolocation.getCurrentPosition();
+    setLoc(coordinates);
+  }, []);
+
   return (
     <div>
+      <h1>Geolocation</h1>
+      <p>Your location is:</p>
+      <p>Latitude: {loc?.coords.latitude}</p>
+      <p>Longitude: {loc?.coords.longitude}</p>
+
+      <button onClick={getCurrentPosition}>
+        Get Current Location
+      </button>
     </div>
   );
 }
